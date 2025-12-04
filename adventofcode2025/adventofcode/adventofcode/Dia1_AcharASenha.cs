17d2ac30948f663312252b3fa22540c68f2cdc70
Jonathan Iha <58711422+JapaTech@users.noreply.github.com>
2025-12-04 09:06:58 -0300

    Dia 1, primeira metade - completo
    Resultado 1031

---
+++ adventofcode2025/adventofcode/adventofcode/Dia1_AcharASenha.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace adventofcode;
+
+public static class Dia1_AcharASenha
+{
+    private static char[] chars = ['R', 'r', 'L', 'l'];
+
+    public static int AcharSenha(int valorInicial, string codigo)
+    {
+        int solucao = 0;
+
+        if (codigo.Length == 0)
+        {
+            return 0;
+        }
+
+        while(codigo.Length > 0)
+        {
+            char direcao = codigo[codigo.IndexOfAny(chars)];
+            string codigoSemDirecao = codigo.Substring(codigo.IndexOf(direcao) + 1);
+            int casasParaPegar = codigoSemDirecao.IndexOfAny(chars) == -1 ? codigoSemDirecao.Length : codigoSemDirecao.IndexOfAny(chars);
+            string valor = codigoSemDirecao.Substring(0, casasParaPegar);
+            codigo = codigo.Remove(0, casasParaPegar + 1);
+
+            Console.WriteLine("---------------");
+            Console.WriteLine("Direcao: " + direcao);
+            Console.WriteLine("Codigo sem direcao: " + codigoSemDirecao);
+            Console.WriteLine("Valor:" + valor);
+            Console.WriteLine("Codigo restante:" + codigo);
+            Console.WriteLine();
+
+            int valorInt = int.Parse(valor);
+
+            if (valorInt >= 100)
+            {
+                valorInt = valorInt % 100;
+
+            }
+
+            int resultado = 0;
+            if (direcao == 'R' || direcao == 'r')
+            {
+                resultado = valorInicial + valorInt;
+                resultado = resultado > 100 ? resultado -= 100 : resultado;
+
+            }
+            else if (direcao == 'L' || direcao == 'l')
+            {
+                resultado = valorInicial - valorInt;
+                resultado = resultado < 0 ? resultado += 100 : resultado;
+            }
+            Console.WriteLine("Resultado: " + resultado);
+
+            if (resultado == 0 || resultado == 100 || resultado == -100)
+            {
+                solucao++;
+                resultado = 0;
+            }            
+            valorInicial = resultado;            
+        }        
+        return solucao;            
+    }
+}
